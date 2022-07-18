f8b0cc0d0a409a7e43ee094a67246328be222518
spacegoat1 <yash_b_mehta@brown.edu>
2022-07-18 05:30:07 -0400

    update allowed hosts

--- music_rec/music_rec/settings.py
+++ music_rec/music_rec/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g@+^ag_iew#jh(n+xz_6#)m1taur+)!#(p*--c+d)61udw0q79'
+SECRET_KEY = os.getenv("DSK_MR")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
